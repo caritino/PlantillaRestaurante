7c7ea4fcfcb208614a903f3271dbfc97eae2867d
Alejandro Rabadán Camacho <arabadan70@hotmail.com>
2016-07-06 04:17:53 -0500

    Working in the scrolling JS

--- js/main.js
+++ js/main.js
@@ -5,8 +5,32 @@
 	console.log($(window).height());
 
 	$(window).scroll(function(){
-		console.log(isInBottom());
+		const inBottom = isInBottom()
+
+		if(inBottom && !sticky){
+			// Mostrar la navegacion sticky
+			console.log("Cambiar la navegación")
+			sticky = true
+			stickNavigation()
+		}else if(!inBottom && sticky){
+			// Ocultar la navegacion sticky
+			console.log("Regresar la navegación")
+			sticky = false
+			unStickNavigation()
+		}
 	})
+
+	function stickNavigation(){
+		$("#description").addClass("fixed").removeClass("absolute")
+		$("#navigation").addClass("hidden")
+		$("#sticky-navigation").removeClass("hidden")
+	}
+
+	function unStickNavigation(){
+		$("#description").removeClass("fixed").addClass("absolute")
+		$("#navigation").removeClass("hidden")
+		$("#sticky-navigation").addClass("hidden")
+	}
 
 	function isInBottom(){
 		const $description = $("#description")
