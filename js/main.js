96182c0c6aa955954c1d83d6ba9572402d290ea8
Alejandro Rabadán Camacho <arabadan70@hotmail.com>
2016-07-06 04:35:11 -0500

    Adding animations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 	let sticky = false
 
+	$("#sticky-navigation").removeClass("hidden")
+	$("#sticky-navigation").slideUp(0)
+
 	console.log($(window).height());
 
 	$(window).scroll(function(){
@@ -22,21 +25,21 @@
 
 	function stickNavigation(){
 		$("#description").addClass("fixed").removeClass("absolute")
-		$("#navigation").addClass("hidden")
-		$("#sticky-navigation").removeClass("hidden")
+		$("#navigation").slideUp("fast")
+		$("#sticky-navigation").slideDown("fast")
 	}
 
 	function unStickNavigation(){
 		$("#description").removeClass("fixed").addClass("absolute")
-		$("#navigation").removeClass("hidden")
-		$("#sticky-navigation").addClass("hidden")
+		$("#navigation").slideDown("fast")
+		$("#sticky-navigation").slideUp("fast")
 	}
 
 	function isInBottom(){
 		const $description = $("#description")
 		const descriptionHeight = $description.height()
 
-		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 1.5)
+		return $(window).scrollTop() > $(window).height() - (descriptionHeight * 2)
 
 	}
 
